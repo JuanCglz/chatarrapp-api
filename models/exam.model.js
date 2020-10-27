ec5ea34049d55622334fc165e0e20494fff6b549
Juan Carlos <juanc.gonzalez3@gmail.com>
2020-10-26 19:08:48 -0500

    Update on users and reports

--- models/exam.model.js
+++ models/exam.model.js
@@ -2,14 +2,23 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
+const imageSchema = new Schema({
+    imageURL : { type: String, required: true },
+    classification : { type: String, required: true },
+}, {
+    timestamps: true,
+});
+
 const examSchema = new Schema({
     examName: { type: String, required: true },
-    images : [String],
+    images :[{type: Schema.Types.ObjectId, ref: 'Image'}],
     description: { type: String},
 }, {
     timestamps: true,
 });
 
 const Exam = mongoose.model('Exam', examSchema);
+const Image = mongoose.model('Image', imageSchema);
 
 module.exports = Exam;
+module.exports = Image;
